0b152f1ce4f0a4288d8edec3972db68e2253f5c6
Adrian Majcher <majcherrr87@gmail.com>
2024-10-07 23:19:59 +0200

    home work

--- HelloApp/src/app/app.component.ts
+++ HelloApp/src/app/app.component.ts
@@ -1,27 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooComponent } from './foo/foo.component';
+import { MyIpComponent } from './my-ip/my-ip.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooComponent],
+  imports: [RouterOutlet, MyIpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  value: string = 'Lorem ipsum';
-  users: string[] = ['Adrian', 'Ilona'];
-  visible: boolean = true;
-
-  changeValue() {
-    this.value = 'Dolor Sit amet';
-  }
-  addUsers() {
-    // this.users.push((Math.random() * 1000).toFixed(2));
-    this.users = [...this.users, (Math.random() * 1000).toFixed(2)];
-  }
-  toggleVisible() {
-    this.visible = !this.visible;
-  }
-}
+export class AppComponent {}
--- HelloApp/src/app/my-ip/my-ip.component.ts
+++ HelloApp/src/app/my-ip/my-ip.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+type IpData = {
+  ip: string;
+};
 
 @Component({
   selector: 'app-my-ip',
   standalone: true,
   imports: [],
   templateUrl: './my-ip.component.html',
-  styleUrl: './my-ip.component.scss'
+  styleUrl: './my-ip.component.scss',
 })
-export class MyIpComponent {
+export class MyIpComponent implements OnInit {
+  isLoading: boolean = true;
+  ip: string = '';
 
+  ngOnInit() {
+    this.getMyIp();
+  }
+
+  async getMyIp() {
+    const response = await fetch('https://api.ipify.org/?format=json');
+    const data = (await response.json()) as IpData;
+    console.log(data.ip);
+    this.ip = data.ip;
+    this.isLoading = false;
+  }
 }
