667e693141b06f10a7f0f140b16bf733d5f983a6
Adrian Majcher <majcherrr87@gmail.com>
2024-10-07 16:16:24 +0200

    exercise

--- HelloApp/src/app/app.component.ts
+++ HelloApp/src/app/app.component.ts
@@ -1,15 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RandomNumbersComponent } from './random-numbers/random-numbers.component';
-import { CarsComponent } from './cars/cars.component';
+import { LazyComponent } from './lazy/lazy.component';
+
+type Box = {
+  r: number;
+  g: number;
+  b: number;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CarsComponent],
+  imports: [RouterOutlet, LazyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'HelloApp';
+  boxes: Box[] = [];
+
+  addBox() {
+    const newBox: Box = {
+      r: Math.round(Math.random() * 255),
+      g: Math.round(Math.random() * 255),
+      b: Math.round(Math.random() * 255),
+      x: Math.round(Math.random() * 800),
+      y: Math.round(Math.random() * 800),
+      width: Math.round(Math.random() * 100) + 100,
+      height: Math.round(Math.random() * 100) + 100,
+    };
+    this.boxes.push(newBox);
+  }
+  getStyle(box: Box) {
+    return {
+      backgroundColor: `rgb(${box.r},${box.g},${box.b})`,
+      height: `${box.height}px`,
+      width: `${box.width}px`,
+      left: `${box.x}px`,
+      top: `${box.y}px`,
+    };
+  }
 }
