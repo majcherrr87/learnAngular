28a49395e2a6a3de4ed30b25f1ba02bc3a1ed426
Adrian Majcher <majcherrr87@gmail.com>
2024-10-07 21:44:06 +0200

    home work

--- HelloApp/src/app/app.component.ts
+++ HelloApp/src/app/app.component.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ContainerComponent } from './container/container.component';
-import { PageContainerComponent } from './page-container/page-container.component';
-import { PageBodyComponent } from './page-body/page-body.component';
-import { PageTitleComponent } from './page-title/page-title.component';
+import { PageLayoutComponent } from './page-layout/page-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    ContainerComponent,
-    PageContainerComponent,
-    PageBodyComponent,
-    PageTitleComponent,
-  ],
+  imports: [RouterOutlet, PageLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ HelloApp/src/app/triple-projection/triple-projection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-triple-projection',
+  standalone: true,
+  imports: [],
+  templateUrl: './triple-projection.component.html',
+  styleUrl: './triple-projection.component.scss'
+})
+export class TripleProjectionComponent {
+
+}
