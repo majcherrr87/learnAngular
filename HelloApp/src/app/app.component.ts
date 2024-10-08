1df5bcffa99bbd971e6e92ac587c393174aa3d46
Adrian Majcher <majcherrr87@gmail.com>
2024-10-08 19:43:28 +0200

    home work

--- HelloApp/src/app/app.component.ts
+++ HelloApp/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NumbersComponent } from './numbers/numbers.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NumbersComponent],
+  imports: [RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
