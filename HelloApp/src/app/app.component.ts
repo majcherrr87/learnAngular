1ba62aafc8f67b9166f4fbecfa3dd5b30dcf2f6c
Adrian Majcher <majcherrr87@gmail.com>
2024-10-08 17:42:27 +0200

    exercise

--- HelloApp/src/app/app.component.ts
+++ HelloApp/src/app/app.component.ts
@@ -1,13 +1,5 @@
-import {
-  Component,
-  computed,
-  effect,
-  Signal,
-  signal,
-  WritableSignal,
-} from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { interval } from 'rxjs';
 import { NumbersComponent } from './numbers/numbers.component';
 
 @Component({
@@ -17,32 +9,4 @@ import { NumbersComponent } from './numbers/numbers.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  counter: WritableSignal<number> = signal(100);
-  doubleCounter: Signal<number> = computed(() => this.counter() * 2);
-
-  constructor() {
-    effect((onCleanUp) => {
-      document.title = this.counter().toString();
-      // console.log(`Counter value: ${this.counter()}`);
-      const interval = setInterval(() => {
-        console.log('TICK');
-      }, this.counter() * 10);
-
-      onCleanUp(() => {
-        clearInterval(interval);
-      });
-    });
-  }
-
-  increase() {
-    this.counter.update((prev) => prev + 1);
-  }
-  decrease() {
-    this.counter.update((prev) => prev - 1);
-  }
-
-  reset() {
-    this.counter.set(100);
-  }
-}
+export class AppComponent {}
--- HelloApp/src/app/numbers/numbers.component.ts
+++ HelloApp/src/app/numbers/numbers.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 
 @Component({
   selector: 'app-numbers',
   standalone: true,
   imports: [],
   templateUrl: './numbers.component.html',
-  styleUrl: './numbers.component.scss'
+  styleUrl: './numbers.component.scss',
 })
 export class NumbersComponent {
+  constructor() {
+    effect(() => {
+      console.log(`Numbers of even numbers: ${this.eventNumbers().length}`);
+    });
+  }
+  numbers: WritableSignal<number[]> = signal([]);
+  eventNumbers: Signal<number[]> = computed(() =>
+    this.numbers().filter((val) => val % 2 === 0)
+  );
 
+  addNewNumber() {
+    const newNumber = Math.round(Math.random() * 10000);
+    this.numbers.update((prev) => [...prev, newNumber]);
+  }
 }
